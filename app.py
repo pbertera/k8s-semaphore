209ad8f5728f8d29b43f9ad3aa8b058b81cfa5fd
Pietro Bertera <pietro@bertera.it>
2020-08-20 17:36:15 +0200

    ADD: annotation support

--- app.py
+++ app.py
@@ -10,16 +10,52 @@ urls = (
 )
 
 if 'DEBUG' in os.environ.keys():
+    web.config.debug = True
     debug = True
 else:
+    web.config.debug = False
     debug = False
+if 'VALIDATION_ANNOTATION' in os.environ:
+    validation_annotation = os.environ['VALIDATION_ANNOTATION']
+else:
+    validation_annotation = 'bertera.it/k8s-semaphore'
+if 'RED_LIGHT__ANNOTATION' in os.environ:
+    red_light_annotation = os.environ['RED_LIGHT_ANNOTATION']
+else:
+    red_light_annotation = 'bertera.it/k8s-semaphore/red'
 
 class validate:
     def POST(self):
         request = json.loads(web.data())
-        uid = request['request']['uid']
         if debug:
             print("/validate REQ: %s" % request)
+        
+        uid = request['request']['uid']
+        annotations = request['request']['oldObject']['metadata']['annotations']
+        resource_name = request['request']['name']
+        resource_kind = request['request']['requestKind']['kind']
+        resource_version = request['request']['requestKind']['version']
+        resource_group = request['request']['requestKind']['group']
+        
+        if validation_annotation in annotations:
+            if red_light_annotation in annotations:
+                response = {
+
+                    "apiVersion": "admission.k8s.io/v1",
+                    "kind": "AdmissionReview",
+                    "response": {
+                        "uid": uid,
+                        "allowed": False,
+                        "status": {
+                            "code": 403,
+                            "message": "Resource %s (kind: %s, version: %s, group: %s) is annotated with %s, cannot be removed" % (resource_name, resource_version, resource_group, red_light_annotation)
+                            }
+                        }
+                    } 
+                if debug:
+                    print("/validate RES: %s" % response)
+                return json.dumps(response)
+        
         response = {
             "apiVersion": "admission.k8s.io/v1",
             "kind": "AdmissionReview",
